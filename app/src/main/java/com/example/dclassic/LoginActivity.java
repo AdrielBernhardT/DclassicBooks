3cb608594066898efe6e75d14c2ce2af12aacfc5
ReygaDirga <reygadirg@gmail.com>
2026-04-23 21:18:08 +0700

    update page:minor

--- app/src/main/java/com/example/dclassic/LoginActivity.java
+++ app/src/main/java/com/example/dclassic/LoginActivity.java
@@ -1,24 +1,29 @@
 package com.example.dclassic;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class LoginActivity extends AppCompatActivity {
+    Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        startBtn = findViewById(R.id.startBtn);
+
+        startBtn.setOnClickListener(v -> {
+
+            Intent intent =
+                    new Intent(LoginActivity.this,
+                            HomeActivity.class);
+
+            startActivity(intent);
+
         });
     }
 }
