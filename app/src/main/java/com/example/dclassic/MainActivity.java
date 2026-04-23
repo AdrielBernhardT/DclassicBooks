53699c1caa2f0c1f6105950c79854c88d959d4cb
ReygaDirga <reygadirg@gmail.com>
2026-04-23 11:27:42 +0700

    first commit

---
+++ app/src/main/java/com/example/dclassic/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.dclassic;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.startBtn);
+
+        startBtn.setOnClickListener(v -> {
+
+            Intent intent =
+                    new Intent(MainActivity.this,
+                            LoginActivity.class);
+
+            startActivity(intent);
+
+        });
+    }
+}
